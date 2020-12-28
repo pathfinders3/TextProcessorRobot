a6acbd7f42b6dfb6724bcafe1cf64dd4b4269a8a
pathfinders3 <56064936+pathfinders3@users.noreply.github.com>
2020-12-28 17:33:02 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace TextProcessorRobot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeEmptyLinesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblVowels1 = new System.Windows.Forms.Label();
+            this.lblVowels2 = new System.Windows.Forms.Label();
+            this.lblConso2 = new System.Windows.Forms.Label();
+            this.lblConso1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 118);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(543, 141);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.removeEmptyLinesToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // removeEmptyLinesToolStripMenuItem
+            // 
+            this.removeEmptyLinesToolStripMenuItem.Name = "removeEmptyLinesToolStripMenuItem";
+            this.removeEmptyLinesToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F8;
+            this.removeEmptyLinesToolStripMenuItem.Size = new System.Drawing.Size(248, 26);
+            this.removeEmptyLinesToolStripMenuItem.Text = "Remove Empty Lines";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 268);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(543, 141);
+            this.textBox2.TabIndex = 2;
+            // 
+            // lblVowels1
+            // 
+            this.lblVowels1.AutoSize = true;
+            this.lblVowels1.Location = new System.Drawing.Point(12, 72);
+            this.lblVowels1.Name = "lblVowels1";
+            this.lblVowels1.Size = new System.Drawing.Size(82, 15);
+            this.lblVowels1.TabIndex = 3;
+            this.lblVowels1.Text = "ㅓㅕㅣㅏㅑ";
+            // 
+            // lblVowels2
+            // 
+            this.lblVowels2.AutoSize = true;
+            this.lblVowels2.Location = new System.Drawing.Point(12, 49);
+            this.lblVowels2.Name = "lblVowels2";
+            this.lblVowels2.Size = new System.Drawing.Size(82, 15);
+            this.lblVowels2.TabIndex = 4;
+            this.lblVowels2.Text = "ㅗㅛㅡㅜㅠ";
+            // 
+            // lblConso2
+            // 
+            this.lblConso2.AutoSize = true;
+            this.lblConso2.Location = new System.Drawing.Point(128, 73);
+            this.lblConso2.Name = "lblConso2";
+            this.lblConso2.Size = new System.Drawing.Size(82, 15);
+            this.lblConso2.TabIndex = 6;
+            this.lblConso2.Text = "ㅁㄴㅇㄹㅎ";
+            // 
+            // lblConso1
+            // 
+            this.lblConso1.AutoSize = true;
+            this.lblConso1.Location = new System.Drawing.Point(128, 48);
+            this.lblConso1.Name = "lblConso1";
+            this.lblConso1.Size = new System.Drawing.Size(82, 15);
+            this.lblConso1.TabIndex = 5;
+            this.lblConso1.Text = "ㅂㅈㄷㄱㅅ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblConso2);
+            this.Controls.Add(this.lblConso1);
+            this.Controls.Add(this.lblVowels2);
+            this.Controls.Add(this.lblVowels1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Text Processor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeEmptyLinesToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblVowels1;
+        private System.Windows.Forms.Label lblVowels2;
+        private System.Windows.Forms.Label lblConso2;
+        private System.Windows.Forms.Label lblConso1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextProcessorRobot
+{
+    public partial class Form1 : Form
+    {
+        enum TypeMode { Con, Vow };
+        enum EditMode { Edit, Progress };
+
+        struct jaso
+        {
+            public string value;    // 'ㄱ'...
+            public string cve;      // c.
+            //public jaso() { value = null; cve = null; }
+        }
+
+        TypeMode g_typeMode = TypeMode.Con;
+        EditMode g_editMode = EditMode.Progress;
+
+        Stack<jaso> g_typed = new Stack<jaso>();
+
+        jaso g_ch1;
+        jaso g_ch2;
+        Queue<string> g_queEdit = new Queue<string>();  // Editing Chars
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// input alphabet and return han conso.
+        /// </summary>
+        /// <param name="ch1">Keys.R, so : qwert asdfg zxcvb</param>
+        /// <returns></returns>
+        string GetConso(Keys key)
+        {
+            string ret;
+
+            switch (key)
+            {
+                case Keys.R: ret = "ㄱ"; break;
+                case Keys.T: ret = "ㅅ"; break;
+                case Keys.E: ret = "ㄷ"; break;
+                case Keys.W: ret = "ㅈ"; break;
+                case Keys.Q: ret = "ㅂ"; break;
+
+                case Keys.A: ret = "ㅁ"; break;
+                case Keys.S: ret = "ㄴ"; break;
+                case Keys.D: ret = "ㅇ"; break;
+                case Keys.F: ret = "ㄹ"; break;
+                case Keys.G: ret = "ㅎ"; break;
+
+                default: ret = "XX"; break;
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// return if it conso or vow
+        /// </summary>
+        /// <param name="con"></param>
+        /// <returns></returns>
+        string IsCorV(string con)
+        {
+            switch (con)
+            {
+                case "ㄱ": case "ㄴ": case "ㄷ":
+                case "ㄹ": case "ㅁ": case "ㅂ":
+                case "ㅅ": case "ㅈ": case "ㅊ":
+                case "ㅋ": case "ㅌ": case "ㅍ":
+                case "ㅎ": case "ㄲ": case "ㄸ":
+                case "ㅃ": case "ㅆ": case "ㅉ":
+                    return "c";
+                case "ㅏ": case "ㅑ": case "ㅓ":
+                case "ㅕ": case "ㅗ": case "ㅛ":
+                case "ㅜ": case "ㅠ": case "ㅡ":
+                case "ㅣ": case "ㅐ": case "ㅒ":
+                case "ㅔ": case "ㅖ": 
+                    return "v";
+                default:
+                    return "e";
+            }
+        }
+
+        string GetVowel(Keys key)
+        {
+            string ret;
+
+            switch (key)
+            {
+                case Keys.W: ret = "ㅗ"; break;
+                case Keys.E: ret = "ㅛ"; break;
+                case Keys.R: ret = "ㅜ"; break;
+                case Keys.T: ret = "ㅠ"; break;
+
+                case Keys.S: ret = "ㅓ"; break;
+                case Keys.D: ret = "ㅕ"; break;
+                case Keys.F: ret = "ㅏ"; break;
+                case Keys.G: ret = "ㅑ"; break;
+
+                case Keys.V: ret = "ㅡ"; break;
+                case Keys.B: ret = "ㅣ"; break;
+
+                case Keys.X: ret = "ㅐ"; break;
+                case Keys.C: ret = "ㅔ"; break;
+
+                default: ret = "XX"; break;
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Pop Jaso Temporarilly
+        /// </summary>
+        /// <returns></returns>
+        jaso PopTemp()
+        {
+            jaso ret = new jaso();
+            ret.cve = null;
+            ret.value = null;
+
+            if (g_typed.Count > 0)
+            {
+                ret = g_typed.Pop();
+
+                return ret;
+            }
+            return ret;    // return NULL if Nothing to Pop
+        }
+
+        void PushBack(string ch1, string cve)
+        {
+            jaso ret = new jaso();
+            ret.cve = cve;
+            ret.value = ch1;
+
+            g_typed.Push(ret);
+        }
+        void PushBack(jaso j1)
+        {
+            g_typed.Push(j1);
+        }
+
+        /// <summary>
+        /// Change type mode (to c, or v)
+        /// </summary>
+        /// <param name="mode"></param>
+        void ChangeMode(TypeMode mode)
+        {
+            g_typeMode = mode;
+            if (mode == TypeMode.Con)
+            {   // change label
+                lblConso1.BackColor = Color.Red;
+                lblConso2.BackColor = Color.Red;
+                lblVowels1.BackColor = Color.LightGray;
+                lblVowels2.BackColor = Color.LightGray;
+            }
+            else
+            {
+                lblConso1.BackColor = Color.LightGray;
+                lblConso2.BackColor = Color.LightGray;
+                lblVowels1.BackColor = Color.Red;
+                lblVowels2.BackColor = Color.Red;
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (g_editMode == EditMode.Edit)
+            {
+            }
+            else
+            {   // Progress (Not Editing)
+                if (e.KeyCode == Keys.Space)
+                {
+                    g_typeMode = TypeMode.Con;
+                }
+                else if (e.KeyCode == Keys.Back)
+                {
+                    jaso ch1 = PopTemp();
+                    jaso ch2 = PopTemp();
+                    Console.WriteLine("Ch2-1: {0}, {1}", ch2.value, ch1.value);
+
+                    PushBack(ch1.value, ch1.cve);
+
+                    textBox2.Text = string.Join("", g_typed.ToArray());
+                }
+                else if (e.KeyCode == Keys.D3)
+                {
+                    jaso ch1 = PopTemp();
+                    jaso ch2 = PopTemp();
+
+                    if (ch1.value == null || ch2.value == null)
+                        Console.WriteLine("Null No! Stack has {0}", g_typed.Count);
+                    else
+                    {
+                        Console.WriteLine("Ch2-1: {0}, {1}", ch2.value, ch1.value);
+
+                        // PUSH BACK AFTER EDIT
+                        PushBack(ch2);
+                        PushBack(ch1);
+                    }
+                }
+                else if (g_typeMode == TypeMode.Con)
+                {
+                    Console.WriteLine("Beat it");
+                    textBox2.Text += GetConso(e.KeyCode);
+
+                    jaso ch1;
+                    ch1.value = GetConso(e.KeyCode);
+                    ch1.cve = IsCorV(ch1.value);
+                    g_typed.Push(ch1);
+
+                    ChangeMode(TypeMode.Vow);
+                }
+                else
+                {
+                    Console.WriteLine("Voo Woo");
+                    textBox2.Text += GetVowel(e.KeyCode);
+
+                    jaso ch1;
+                    ch1.value = GetVowel(e.KeyCode);
+                    ch1.cve = IsCorV(ch1.value);
+                    g_typed.Push(ch1);
+
+                    ChangeMode(TypeMode.Con);
+                }
+            }
+
+        }
+    }
+}
